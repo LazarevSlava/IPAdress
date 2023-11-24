22a53f3987eefc80b2f96833fcc48977ef5a4a03
LazarevSlava <549132w@gmail.com>
2023-11-24 09:56:37 +0200

    info panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Search from './components/search_form/Search.jsx';
+import Map from './components/map/map.jsx';
 
 function App() {
   const handleSearch = (value) => {
     console.log(value);
   };
-  return <Search onSearch={handleSearch} />;
-}
+  return (
+    <div className='header'>
+    <Search onSearch={handleSearch} />
+  <Map />
+  </div>
+)
+  }
 
 export default App;
--- src/components/search_form/Search.jsx
+++ src/components/search_form/Search.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../button/Button.jsx';
+import style from './search.module.scss';
 
 function Search(props) {
   const [inputValue, setInputValue] = useState('');
@@ -11,9 +12,9 @@ function Search(props) {
   };
 
   return (
-    <div className="search">
+    <div className={style.search}>
       <input
-        className="input"
+        className={style.input}
         type="text"
         placeholder={'Search for any IP address or domain'}
         value={inputValue}
@@ -21,7 +22,7 @@ function Search(props) {
       />
       <Button onClick={handleButtonClick}>
         <svg
-          className="arrow"
+          className={style.arrow}
           xmlns="http://www.w3.org/2000/svg"
           height="2em"
           viewBox="0 0 320 512"
