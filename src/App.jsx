a7968f62685e3a8eb668bb89f5a3719fbf9c5baf
LazarevSlava <549132w@gmail.com>
2023-11-26 19:10:36 +0200

    added props deleted editorconfig file

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='header'>
     <Search onSearch={handleSearch} />
-  <Map />
+  <Map ip="mmm" location='mmm' timezone='mmm' isp='mmm'/>
   </div>
 )
   }
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,15 +1,24 @@
 import style from './map.module.scss';
 
-const Map = () => {
+
+ const Map = (props) => {
   return (
     <div className={style.infoPanel}>
-      <div className={style.IPAddress}>IP ADDRESS</div>
+      <div className={style.IPAddress}>
+        IP ADDRESS<h3 className={style.params}>
+          {props.ip}</h3></div>
       <hr />
-      <div className={style.location}>LOCATION</div>
+      <div className={style.location}>LOCATION
+      <h3 className={style.params}>
+          {props.location}</h3></div>
       <hr />
-      <div className={style.timezone}>TIMEZONE</div>
+      <div className={style.timezone}>TIMEZONE<h3 className={style.params}>
+          {props.timezone}</h3>
+          </div>
       <hr />
-      <div className={style.ISP}>ISP</div>
+      <div className={style.ISP}>ISP
+      <h3 className={style.params}>
+          {props.isp}</h3></div>
     </div>
   );
 };
