c455b14561e4c7a5c4a4a23b9a5b5958b5e6f294
LazarevSlava <549132w@gmail.com>
2023-11-28 13:02:53 +0200

    Add leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css';
 import Search from './components/search_form/Search.jsx';
 import Map from './components/map/map.jsx';
+import Leaflet_map from './components/leaflet/Leaflet_map';
 
-const isLoading=true;
+const isLoading=false;
 
 function App() {
   const handleSearch = (value) => {
     console.log(value);
   };
   return (
+    <div>
     <div className='header'>
     <Search onSearch={handleSearch} />
   <Map ip="mmm" location='mmm' timezone='mmm' isp='mmm' isLoading={isLoading}/>
+  </div>
+  <Leaflet_map/>
   </div>
 )
   }
---
+++ src/components/leaflet/Leaflet_map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet';
+import style from './leaflet.module.scss';
+import Search from '../search_form/Search';
+
+const Leaflet_map = () => {
+  return (
+    <MapContainer className={style.mapCont} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  )
+}
+
+export default Leaflet_map
