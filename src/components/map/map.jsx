8c1d20f9801bbcb557fb7ae3428b42b70e32bce3
LazarevSlava <134292556+LazarevSlava@users.noreply.github.com>
2023-12-02 19:03:54 +0200

    Update map.jsx

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,26 +1,17 @@
-import style from './map.module.scss';
+import './App.css';
+import Search from './components/search_form/Search.jsx';
+import Map from './components/map/map.jsx';
 
-
- const Map = (props) => {
+function App() {
+  const handleSearch = (value) => {
+    console.log(value);
+  };
   return (
-    <div className={style.infoPanel}>
-      <div className={style.IPAddress}>
-        IP ADDRESS<h3 className={style.params}>
-          {props.ip}</h3></div>
-      <hr />
-      <div className={style.location}>LOCATION
-      <h3 className={style.params}>
-          {props.location}</h3></div>
-      <hr />
-      <div className={style.timezone}>TIMEZONE<h3 className={style.params}>
-          {props.timezone}</h3>
-          </div>
-      <hr />
-      <div className={style.ISP}>ISP
-      <h3 className={style.params}>
-          {props.isp}</h3></div>
-    </div>
-  );
-};
+    <div className='header'>
+    <Search onSearch={handleSearch} />
+  <Map ip="mmm" location='mmm' timezone='mmm' isp='mmm'/>
+  </div>
+)
+  }
 
-export default Map;
+export default App;
