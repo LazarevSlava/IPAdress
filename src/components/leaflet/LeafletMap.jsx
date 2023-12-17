8764ae0f017fd6d956be91de6d986f028a904d6c
LazarevSlava <549132w@gmail.com>
2023-12-17 16:02:25 +0200

    changed icon marker

--- src/components/leaflet/LeafletMap.jsx
+++ src/components/leaflet/LeafletMap.jsx
@@ -1,13 +1,13 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import style from './leaflet.module.scss';
 import { Icon } from 'leaflet';
-import MarkerIcon from '../../assets/images/marker-icon.png';
+import MarkerIcon from '../../assets/images/icon-location.svg';
 import ShadowIcon from '../../assets/images/marker-shadow.png';
 
 const LeafletMap = (props) => {
   const customIcon = new Icon({
     iconUrl: MarkerIcon,
-    iconSize: [32, 32],
+    iconSize: [42, 52],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
     shadowUrl: ShadowIcon,
