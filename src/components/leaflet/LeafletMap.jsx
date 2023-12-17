79a40b274acf87202e6a232de26c9132e19ea98c
LazarevSlava <549132w@gmail.com>
2023-12-17 20:49:28 +0200

    added key for rerender

--- src/components/leaflet/LeafletMap.jsx
+++ src/components/leaflet/LeafletMap.jsx
@@ -2,26 +2,22 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import style from './leaflet.module.scss';
 import { Icon } from 'leaflet';
 import MarkerIcon from '../../assets/images/icon-location.svg';
-import ShadowIcon from '../../assets/images/marker-shadow.png';
+
+const customIcon = new Icon({
+  iconUrl: MarkerIcon,
+  iconSize: [42, 52],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
 
 const LeafletMap = (props) => {
-  const customIcon = new Icon({
-    iconUrl: MarkerIcon,
-    iconSize: [42, 52],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32],
-    shadowUrl: ShadowIcon,
-    shadowSize: [41, 41],
-    shadowAnchor: [12, 41],
-  });
-
   return (
     <MapContainer
+      key={`${props.coordinates[0]}`}
       className={style.mapCont}
       center={props.coordinates}
       zoom={13}
       scrollWheelZoom={false}
-      l
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
