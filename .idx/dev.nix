7a7ab9314f03907f1a26fdbba6af9659aa26b4c3
michey85 <michey85@gmail.com>
2024-07-20 11:35:52 +0000

    idx basic setup

---
+++ .idx/dev.nix
@@ -0,0 +1,22 @@
+{ pkgs }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
